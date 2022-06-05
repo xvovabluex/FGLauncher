8ee3bc434adf2384c099ced39711d812071a8dcc
xvovabluex <95418222+xvovabluex@users.noreply.github.com>
2022-06-05 12:57:18 +0300

    Add files via upload

---
+++ b1.5_01.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+cd /d %~dp0
+mkdir b1.5_01
+Call :UnZipFile "b1.5_01" "b1.5_01.zip"
+exit /b
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
